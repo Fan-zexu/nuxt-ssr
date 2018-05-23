05920c548f43b537fe7c773eb0b119794e4ec583
zhangnan <zhangnan@zhiketong.cn>
2018-05-23 18:30:18 +0800

    动态路由和参数校验

---
+++ pages/news/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>new-content: {{$route.params.id}}</h1>
+    <li>
+      <nuxt-link :to="{name: 'index'}">HOME</nuxt-link>
+    </li>
+  </div>
+</template>
+
+<script>
+  export default {
+    // 校验动态路由参数
+    validate ({ params }) {
+      return /\d+/.test(params.id)
+    },
+    data () {
+      return {
+        title: this.$route.params.title
+      }
+    },
+    // 独立设置head信息
+    head () {
+      return {
+        title: this.title,
+        meta: [
+          { hid: 'description', name: 'news', content: 'This is news page' }
+        ]
+      }
+    }
+  }
+</script>
