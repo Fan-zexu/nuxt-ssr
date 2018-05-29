a3656b5af81f1968ea5ddfe8292cddf56476db84
zhangnan <zhangnan@zhiketong.cn>
2018-05-29 18:30:38 +0800

    asyncData异步获取数据

---
+++ pages/asyncData.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>asyncData</h1>
+    <p>{{info.name}}</p>
+    <p>{{info.age}}</p>
+    <p>{{info.render}}</p>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  
+  export default {
+    data () {
+      return {
+        // info: {}
+        name:'hello World'
+      }
+    },
+    async asyncData () {
+      let {data} = await axios.get('https://api.myjson.com/bins/pgsv2')
+      return {
+        info: data
+      }
+    }
+  }
+</script>
